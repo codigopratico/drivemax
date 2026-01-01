a0cd3bf77cfd90f6bda87575a6493d65aa0f2fae
Kauan Gabriel <fandaart.75@gmail.com>
2026-01-01 12:08:45 -0300

    Atualizando base do caminho

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/DriveMAX/',
+  base: '/drivemax/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
